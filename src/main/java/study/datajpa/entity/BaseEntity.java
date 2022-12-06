c14085be0e5b23d91c35394f135044e342cbf87d
공수한 <ksh92315@pionnet.co.kr>
2022-12-06 12:14:00 +0900

    ToString(onlyExplicitlyIncluded) 관련 주석추가

--- src/main/java/study/datajpa/entity/BaseEntity.java
+++ src/main/java/study/datajpa/entity/BaseEntity.java
@@ -1,6 +1,7 @@
 package study.datajpa.entity;
 
 import lombok.Getter;
+import lombok.ToString;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -12,8 +13,10 @@ import javax.persistence.MappedSuperclass;
 @EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
 @Getter
+@ToString(onlyExplicitlyIncluded = true)//onlyExplicitlyIncluded = true 일 떄는 @ToString.Include가 붙은 필드만 출력 가능하다
 public class BaseEntity extends BaseTimeEntity {
 
+    @ToString.Include
     @CreatedBy
     @Column(updatable = false)
     private String createBy;
@@ -21,6 +24,7 @@ public class BaseEntity extends BaseTimeEntity {
     @LastModifiedBy
 //    @Column(updatable = false)
     private String updateBy;
+
 }
 
 
